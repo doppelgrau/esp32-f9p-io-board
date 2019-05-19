7df9cd324155f8784322588b2a9a6df6bc35535f
Johannes Formann <johannes.formann@uni-duisburg-essen.de>
2019-05-20 00:00:25 +0200

    fixes for small Load-Test

--- code/Load-Test/Load-Test.ino
+++ code/Load-Test/Load-Test.ino
@@ -1,91 +1,79 @@
 // IO pins --------------------------------
-#define LED_INTERNAL 2
-#define LED_EXTERNAL1 32
-#define LED_EXTERNAL2 33
-#define ANALOG_INPUT_WS 39
-#define ANALOG_INPUT_AS 36
-#define ANALOG_INPUT_RESERVE 34
-#define I2C_SDA 21
-#define I2C_SCL 22
 #define VNH_A_PWM 4
 #define VNH_A_E1 0
 #define VNH_A_E2 2
-#define VNH_B_PWM 14
-#define VNH_B_E1 13
-#define VNH_B_E2 12
-#define F9P_RX 16
-#define F9P_TX 17
+#define VNH_B_PWM 12
+#define I2C_SDA 32
+#define I2C_SCL 33
 // includes
-#include "MMA8452_AOG.h"
-#include "Adafruit_ADS1015.h"
 #include <Wire.h>
 
-// global variables
-int check = 1;
-int state = 0;
-
-// instances
-MMA8452 accelerometer;
-Adafruit_ADS1115 ads = Adafruit_ADS1115(0x49);
 
 void setup() {
-  // Setup IO
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_INTERNAL, OUTPUT);
-  pinMode(LED_EXTERNAL1, OUTPUT);
-  pinMode(LED_EXTERNAL2, OUTPUT);
-
   // initialize PINs for VNHs
   pinMode(VNH_A_PWM, OUTPUT);
   pinMode(VNH_A_E1, OUTPUT);
   pinMode(VNH_A_E2, OUTPUT);
   pinMode(VNH_B_PWM, OUTPUT);
-  pinMode(VNH_B_E1, OUTPUT);
-  pinMode(VNH_B_E2, OUTPUT);
-
-  // analog inputs - set input to explicit disable any pullups
-  pinMode(ANALOG_INPUT_WS, INPUT);
-  pinMode(ANALOG_INPUT_AS, INPUT);
-  pinMode(ANALOG_INPUT_RESERVE, INPUT);
-  analogReadResolution(10); // Default of 12 is not very linear. Recommended to use 10 or 11 depending on needed resolution.
-  analogSetAttenuation(ADC_6db); // Default is 11db which is very noisy. Recommended to use 2.5 or 6.
-
-  // I2C
-  Wire.begin(I2C_SDA, I2C_SCL, 400000);
 
   // PWM for outputs
   //   PWM-Channel
-  ledcSetup(0, 1500, 8);
+  ledcSetup(0, 10000, 8);
   //   IO-PINs
   ledcAttachPin(VNH_A_PWM, 0);
   ledcAttachPin(VNH_B_PWM, 0);
 
-  // Serial for F9P
-  Serial2.begin(38400, SERIAL_8N1, F9P_RX, F9P_TX);
+
+
+  // I2C
+  Wire.begin(I2C_SDA, I2C_SCL, 400000);
+
+  // Baustein konfigurieren
+  // direction (Input/Output)
+  setByteI2C(0x43, 0x03, 0b11101111);
+  // disable High-Z on outputs
+  setByteI2C(0x43, 0x07, 0b00010000);
+  // en-/disable Pullup/downs
+  setByteI2C(0x43, 0x0B, 0b00010000);
+  // set direction of the pull
+  setByteI2C(0x43, 0x0D, 0b00010000);
 
 }
 void loop() {
-  int poti = ads.readADC_Differential_0_1();
-  int ref = ads.readADC_Differential_2_3();
+  // duty cyle
+  ledcWrite(0, 230);
+  
+  digitalWrite(VNH_A_E1, HIGH);
+  digitalWrite(VNH_A_E2, LOW);
+  setByteI2C(0x43, 0x05, 0b10100010);
 
-  int output = (poti / (float)ref) * 512 - 256;
+  // warten 0,5s
+  delay(500);
 
-  if (output > 0) {
+  // duty cyle
+  ledcWrite(0, 25);
 
-    ledcWrite(0, output);
+  // und andersherum
+  digitalWrite(VNH_A_E1, LOW);
+  digitalWrite(VNH_A_E2, HIGH);
+  setByteI2C(0x43, 0x05, 0b01000100);
+  // warten 0,5s
+  delay(500);
+}
 
-    digitalWrite(VNH_A_E1, HIGH);
-    digitalWrite(VNH_A_E2, LOW);
-    digitalWrite(VNH_B_E1, HIGH);
-    digitalWrite(VNH_B_E2, LOW);
-  } else {
-    ledcWrite(0, output * -1);
 
-    digitalWrite(VNH_A_E1, LOW);
-    digitalWrite(VNH_A_E2, HIGH);
-    digitalWrite(VNH_B_E1, LOW);
-    digitalWrite(VNH_B_E2, HIGH);
-  }
+uint8_t getByteI2C(int address, int i2cregister) {
+  Wire.beginTransmission(address);
+  Wire.write(i2cregister);
+  Wire.endTransmission(false);
+  uint8_t state = Wire.requestFrom(address, 1, (int)true);
+  return Wire.read();
+}
 
-  delay(100);
+
+uint8_t setByteI2C(int address, byte i2cregister, byte value) {
+  Wire.beginTransmission(address);
+  Wire.write(i2cregister);
+  Wire.write(value);
+  return Wire.endTransmission();
 }
